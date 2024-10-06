5dbacf8df003997c2eb08d708e69f09740a466a2
Ansh Verma <anshv9753@gmail.com>
2024-10-06 14:02:09 +0000

    optamized code

--- Matrix Operation/rotate_image(Clockwise).cpp
+++ Matrix Operation/rotate_image(Clockwise).cpp
@@ -2,74 +2,40 @@
 #include <algorithm>
 
 using namespace std;
-//clockwise 90'
-void rotate(int a[][1000], int n)
-{
-    //to take transpose
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i < j)
-            {
-                swap(a[i][j], a[j][i]);
-            }
+
+// Function to rotate the matrix 90 degrees clockwise
+void rotate(int a[][1000], int n) {
+    // Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {  // Start from j = i + 1 to avoid swapping back
+            swap(a[i][j], a[j][i]);
         }
     }
 
-    //for reversing rows
-    for (int row = 0; row < n; row++)
-    {
-        int start_col = 0;
-        int end_col = n - 1;
-        while (start_col < end_col)
-        {
-            swap(a[row][start_col], a[row][end_col]);
-            start_col++;
-            end_col--;
-        }
+    // Reverse each row
+    for (int row = 0; row < n; row++) {
+        reverse(a[row], a[row] + n);
     }
-
-    
 }
 
-void rotate_stl(int a[][1000], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        reverse(a[i], a[i] + n);
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i < j)
-            {
-                swap(a[i][j], a[j][i]);
-            }
-        }
-    }
-}
-int main()
-{
+int main() {
     int a[1000][1000];
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+    // Input the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
-    //rotate_stl(a, n);
+
+    // Rotate the matrix
     rotate(a, n);
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+
+    // Output the rotated matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << a[i][j] << " ";
         }
         cout << endl;
